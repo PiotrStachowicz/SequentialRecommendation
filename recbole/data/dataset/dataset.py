4974e31b5be00d9023ec8606bbce744b54d9247e
Piotr Stachowicz <337942@uwr.edu.pl>
2025-06-25 17:14:39 +0200

    Updatet preprocessing with mappings and filters

--- recbole/data/dataset/dataset.py
+++ recbole/data/dataset/dataset.py
@@ -242,6 +242,9 @@ class Dataset(object):
             token (str): dataset name.
             dataset_path (str): path of dataset dir.
         """
+        
+        dataset_path = './recbole/data/' + dataset_path
+
         if not os.path.exists(dataset_path):
             self._download()
         self._load_inter_feat(token, dataset_path)
